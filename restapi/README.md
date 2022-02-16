f5ae86c0f79807d3745d0f02b272373ce674fdcd
Fillipedem <fillipedem@gmail.com>
2022-02-16 17:23:19 -0300

    Fix FastAPI

---
+++ restapi/README.md
@@ -0,0 +1,5 @@
+# Classificador Sincere/Insincere
+
+Rest API para classificar textos em **Sincere** e **Insincere**, treinado nos dados da competição do Quora.
+
+A entrada principal é **/run/**. Espera um request_body com os textos a serem analisados.
--- restapi/app.py
+++ restapi/app.py
@@ -6,13 +6,20 @@ from classifier import TextClassifier
 
 print("Starting...")
 
-MODEL_PATH = './model/model_weights'
+MODEL_PATH = './model/model_dict'
 
 print("Loading Models...")
 text_classifier = TextClassifier(MODEL_PATH)
 
 print("Starting RestAPI...")
-app = FastAPI()
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+app = FastAPI(
+    description=description,
+    version="0.0.1"
+)
 
 
 class RequestText(BaseModel):
@@ -22,10 +29,10 @@ class RequestAnswer(BaseModel):
     labels: List[Tuple[str, str]]
 
 
-@app.post("/run", response_form=RequestText)
-def run(request: RequestText):
+@app.post("/run")
+def run(body: RequestText = None):
     ans = []
-    for text in request.texts:
+    for text in body.texts:
         ans.append(
             (text, text_classifier.predict(text))
         )
